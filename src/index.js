6626e157a8c7f20bbbc70bc1415195e867cacb35
9elt <nelt.primary@gmail.com>
2023-06-29 22:38:13 +0200

    anonymous fn

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
-const perScript = () => {
-  if (!window.location.hostname) { return }
-
-  const REG = (F) => {
-    localStorage.setItem("SR", JSON.stringify(F));
-  }
-
+(() => {
   const GET = () => {
-    let res = localStorage.getItem("SR");
+    let res = localStorage.getItem("PS");
     return res ? JSON.parse(res) : [];
   }
 
+  const REG = (F) => localStorage.setItem("PS", JSON.stringify(F));
+
   window.ps = {
-    add: (...f) => {
-      REG([
-        ...GET(),
-        ...f.filter(f => typeof f === "function").map(f => f + "")
-      ]);
-    },
-    eval: () => {
-      GET().forEach(f => eval(f)());
-    },
-    remove: (...indexes) => {
-      indexes.length == 0
-        ? REG([])
-        : REG(GET().filter((_, i) => !indexes.includes(i)));
-    },
-    list: () => {
-      GET().forEach((f, i) => console.log(`${i} | ${f}`));
-    }
+    add: (...f) => REG([
+      ...GET(),
+      ...f.filter(f => typeof f === "function").map(f => f + "")
+    ]),
+    remove: (...indexes) => indexes.length == 0
+      ? REG([])
+      : REG(GET().filter((_, i) => !indexes.includes(i))),
+    eval: () => GET().forEach(f => eval(f)()),
+    list: () => GET().forEach((f, i) => console.log(`${i} | ${f}`))
   };
 
   window.ps.eval();
@@ -36,6 +24,4 @@ const perScript = () => {
     `PerScript evaluated ${GET().length} scripts on ${window.location.hostname}`,
     '\n`ps` object is available in console'
   );
-}
-
-perScript();
+})();
